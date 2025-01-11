d00b2a59713685c33d413eb35f31283d34b4e5ca
SK-IMTIAZ-ALI <81345763+IMTIAZ9085@users.noreply.github.com>
2025-01-11 09:32:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className="App">
-     <div className="text-3xl font-bold text-green-800">Hello world</div>
+     <Body/>
     </div>
   );
 }
 
 export default App;
+
