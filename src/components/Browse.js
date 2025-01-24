1cfedfccb4a4fdb38955ae49526737801b4fe482
SK-IMTIAZ-ALI <81345763+IMTIAZ9085@users.noreply.github.com>
2025-01-24 23:34:14 +0530

    new commit

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
 
 const Browse = () => {
+
+  const fetchMovieData = async()=>{
+    try{
+    const res = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+    const data = await res.json();
+    console.log(data.results);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  useEffect(()=>{
+    fetchMovieData();
+  },[]);
+
+
   return (
     <div>
     <Header/>
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO_URL = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR = "https://dk2dv4ezy246u.cloudfront.net/widgets/sSYVla14U5v6_large.jpg";
+
+export const API_OPTIONS = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTJiZjAxY2YzMzczNGRjZTVkZTk4MTAxZTUwOWI4MiIsIm5iZiI6MTczMzcxODQ3NC42NTI5OTk5LCJzdWIiOiI2NzU2NzFjYTZmMzdmNmU4OWFkYzczODQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.A4WisLvh8XzgtIaiBnUvZDUSd0Vek37GT_Fz2Hl5Sg8'
+      }
+    };
